513e9b59fe3cfd5711fe0c6a4e7b3af823911338
Furkanklcsl <furkanemrekilic29@gmail.com>
2025-04-17 12:25:09 +0300

    ben bi kayınbabama çay koyup geliyim

--- lib/Motor_Controller/motor_controller.h
+++ lib/Motor_Controller/motor_controller.h
@@ -9,7 +9,10 @@ public:
     MotorController(int motor1Pin, int motor2Pin, int motor3Pin, int motor4Pin);
     void begin(); 
     void arm();  // Motorları arm et
-    void updateMotors(int throttle, int rollPID, int pitchPID);  // Motorları güncelle
+    void updateMotors(int throttle, int rollPID, int pitchPID);
+    int lastM1 = 1000, lastM2 = 1000, lastM3 = 1000, lastM4 = 1000;
+    const int SLEW_RATE = 10;     // µs döngü başına izin verilen max değişim
+    const int DEADBAND  = 10;     // µs altındaki değişimleri ihmal et
 
 private:
     Motor motor1, motor2, motor3, motor4;
